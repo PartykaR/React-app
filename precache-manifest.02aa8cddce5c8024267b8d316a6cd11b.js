cc478fe632aaef8ec115e34cd9af34ad2d599216
Rudzik <freelancer.partyka@gmail.com>
2019-05-30 05:26:28 +0200

    Updates

---
+++ precache-manifest.02aa8cddce5c8024267b8d316a6cd11b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/React-app/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/React-app/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/React-app/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "9989a70d9c4bc3883c21",
+    "url": "/React-app/static/js/runtime~main.9989a70d.js"
+  },
+  {
+    "revision": "6d609750ad5966e61d70",
+    "url": "/React-app/static/js/main.6d609750.chunk.js"
+  },
+  {
+    "revision": "e48de2ed48298d84c5eb",
+    "url": "/React-app/static/js/2.e48de2ed.chunk.js"
+  },
+  {
+    "revision": "6d609750ad5966e61d70",
+    "url": "/React-app/static/css/main.64825349.chunk.css"
+  },
+  {
+    "revision": "6c72b24e3ef4bb94e16b9b736cfcab87",
+    "url": "/React-app/index.html"
+  }
+];
